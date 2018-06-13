eb4f9edea6c0bf80ceca45b177b9fae62e5000dc
DomnulCiot <39223137+DomnulCiot@users.noreply.github.com>
2018-06-13 22:29:13 +0300

    2.0

---
+++ privatecegekagoals-60084ad416b21fae2ade8b1babd184f4229977d4/GoalFollower/app/src/main/java/cegeka/goalfollower/ro/goalfollower/Goal.java
@@ -0,0 +1,9 @@
+package cegeka.goalfollower.ro.goalfollower;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Goal implements Serializable{
+    public String desc;
+    public Date dueDate;
+}
